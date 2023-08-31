856d99c7494b7592e5f766f095a24cbb3157b87b
Christian <jesus.chumpitaz@vallegrande.edu.pe>
2023-08-31 10:34:50 -0500

    Add quantity of product

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { findIndex } from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
@@ -6,18 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  counter = 40;
+  counter = [60, 44, 47, 30, 39, 40, 49, 33];
   activeDecrease = true;
+  initialCounter = [...this.counter];
+  quantityBuy = 1;
 
-  increase() {
-    if (this.counter >= 0) {
-      this.counter = this.counter + 40;
+
+  increase(index: number) {
+    if (this.counter[index] >= 0) {
+      this.counter[index] = this.counter[index] + this.initialCounter[index];
+      this.quantityBuy++;
     }
   }
 
-  decrease() {
-    if (this.counter > 40) {
-      this.counter = this.counter - 40;
+  decrease(index: number) {
+    if (this.counter[index] > this.initialCounter[index]) {
+      this.counter[index] = this.counter[index] - this.initialCounter[index];
+      this.quantityBuy--;
     }
   }
 
