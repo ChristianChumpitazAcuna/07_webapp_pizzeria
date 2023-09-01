9eb8f4986fbc3d6132f31327f9e26469b80468d4
Christian <jesus.chumpitaz@vallegrande.edu.pe>
2023-09-01 13:43:25 -0500

    add product a list

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,6 +9,7 @@ import { findIndex } from 'rxjs';
 export class ProductListComponent {
   counter = [60, 44, 47, 30, 39, 40, 49, 33];
   activeDecrease = true;
+  // Crea un array con los datos de un array existente
   initialCounter = [...this.counter];
   quantityBuy = 1;
 
@@ -16,16 +17,43 @@ export class ProductListComponent {
   increase(index: number) {
     if (this.counter[index] >= 0) {
       this.counter[index] = this.counter[index] + this.initialCounter[index];
-      this.quantityBuy++;
+
     }
   }
 
   decrease(index: number) {
     if (this.counter[index] > this.initialCounter[index]) {
       this.counter[index] = this.counter[index] - this.initialCounter[index];
-      this.quantityBuy--;
+
     }
   }
 
+  purchasedProducts: any[] = [];
+
+  buyPizza(index: number) {
+    const product = {
+      precio: this.counter[index]
+    };
+
+    this.purchasedProducts.push(product);
+  }
+
+
+  calculateTotal() {
+    let total = 0;
+    for (const producto of this.purchasedProducts) {
+      total += producto.precio;
+    }
+    return total;
+  }
+
+  modalVisible = false;
+  openModal(){
+    this.modalVisible = true
+  }
+
+  closeModal(){
+    this.modalVisible = false;
+  }
 }
 
